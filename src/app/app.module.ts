748e0e4c89abcd04d130ef8021fa021c0ccdb4ed
ItsMarvoKim <marvin.kithinji@student.moringaschool.com>
2022-04-28 09:14:11 +0300

    relevant imports inserted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
@@ -9,6 +8,7 @@ import { TimeagoPipe } from './services/timeago.service';
 import { QuotesComponent } from './quotes/quotes.component';
 import { HighlightDirective } from './highlight.directive';
 import { FavoriteComponent } from './favorite/favorite.component';
+
 
 @NgModule({
   declarations: [AppComponent, FormComponent, TimeagoPipe, QuotesComponent, HighlightDirective, FavoriteComponent],
